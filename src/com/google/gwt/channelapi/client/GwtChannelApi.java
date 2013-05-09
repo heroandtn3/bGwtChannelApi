aa65fe0db5456d4e41cef28e76c6e925c2aa747d
heroandtn3 <heroandtn3@gmail.com>
2013-05-09 17:49:59 +0700

    add entrypoint to testing

--- src/com/google/gwt/channelapi/client/GwtChannelApi.java
+++ src/com/google/gwt/channelapi/client/GwtChannelApi.java
@@ -1,38 +1,83 @@
 package com.google.gwt.channelapi.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class GwtChannelApi implements EntryPoint {
+
+	private Channel channel;
+	private TextBox tbClientId;
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		Channel channel = new Channel("Sang");
+
+		tbClientId = new TextBox();
+		RootPanel.get().add(tbClientId);
+
+		Button butJoin = new Button("Join");
+		RootPanel.get().add(butJoin);
+		butJoin.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				createChannel(tbClientId.getText());
+			}
+		});
+
+		Button butExit = new Button("Exit");
+		RootPanel.get().add(butExit);
+		butExit.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				channel.close();
+			}
+		});
+
+		Button butSend = new Button("Send message");
+		RootPanel.get().add(butSend);
+		butSend.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				channel.sendMessage(tbClientId.getText(), "Hello World");
+			}
+		});
+
+	}
+
+	private void createChannel(String clientId) {
+		channel = new Channel(clientId);
 		channel.addChannelListener(new ChannelListener() {
-			
+
 			@Override
 			public void onOpen() {
 				System.out.println("on open");
-				
+
 			}
-			
+
 			@Override
 			public void onMessage(String message) {
-				System.out.println("on message");
-				
+				System.out.println("on message: " + message);
+
 			}
-			
+
 			@Override
 			public void onError(int code, String description) {
 				System.out.println("on error");
-				
+
 			}
-			
+
 			@Override
 			public void onClose() {
-				// TODO Auto-generated method stub
-				
+				System.out.println("on close");
+
 			}
 		});
 		channel.join();
